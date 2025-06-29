21339078f2df6112ab434ab737662b29a695ad6d
Mayank Pal <palmayank2100@gmail.com>
2025-06-29 19:01:40 +0530

    Add initial component files and configuration for UI elements

--- apps/web/.prettierrc.js
+++ apps/web/.prettierrc.js
@@ -1,4 +1,7 @@
-/** @type {import("prettier").Config} */
+/** @type {import('prettier').Config} */
 module.exports = {
   ...require('@repo/eslint-config/prettier-base'),
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
 };
