6a9e0e0a24509e9e4d249290b1d41216900eaf5f
Riktam Santra <riktam-santra56@gmail.com>
2025-07-03 21:53:22 +0530

    added redis host and port from config

--- apps/api/src/app_redis/app_redis.module.ts
+++ apps/api/src/app_redis/app_redis.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 import { RedisModule } from '@nestjs-modules/ioredis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [
-        RedisModule.forRoot({
-            options: {
-                host: 'localhost',
-                port: 6379,
-            },
-            type: 'single'
-        }),
-    ],
+  imports: [
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'single',
+        options: {
+          host: configService.get<string>('REDIS_HOST', 'localhost'),
+          port: configService.get<number>('REDIS_PORT', 6379),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppRedisModule {}
