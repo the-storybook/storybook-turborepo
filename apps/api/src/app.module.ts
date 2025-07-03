207cd3e5e7acc47f96a020b45e61a20d83a8a7b5
Riktam Santra <riktam-santra56@gmail.com>
2025-07-03 21:43:58 +0530

    added config package

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { LinksModule } from './links/links.module';
-
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
-
-import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { PasswordEncoderService } from './password_encoder/password_encoder.service';
 import { AuthService } from './auth/auth.service';
@@ -16,9 +15,26 @@ import { OtpModule } from './otp/otp.module';
 import { AppRedisModule } from './app_redis/app_redis.module';
 import { PasswordEncoderModule } from './password_encoder/password_encoder.module';
 
-
 @Module({
-  imports: [LinksModule, MongooseModule.forRoot(process.env.MONGODB_URL), UserModule, AuthModule, OtpModule, AppRedisModule, PasswordEncoderModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the ConfigModule available globally
+      envFilePath: '.env', // Specifies the path to your .env file
+    }),
+    LinksModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    AuthModule,
+    OtpModule,
+    AppRedisModule,
+    PasswordEncoderModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PasswordEncoderService, AuthService, MailerService, OtpService],
 })
